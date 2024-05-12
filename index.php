d9af42c69d140aabbf2d4cdd479c05c76f893b1e
haz10em <haz10em@gmail.com>
2024-05-12 22:10:18 +0300

    write code the php to send the message to me

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  include_once("index.html");
+    // Send the email
+    $to = "alyaarihazem@gmail.com";
+    $headers = "From: $email";
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email.";
+    }
+}
 
+// Include the HTML form
+include_once("index.html");
 ?>
